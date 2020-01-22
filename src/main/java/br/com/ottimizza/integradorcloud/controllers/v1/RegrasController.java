4d77a29110edbc3c1e76382f4bac0a8293d020ac
Lucas Martins <dev.lucasmartins@gmail.com>
2020-01-22 18:44:52 -0300

    added @RequestBody annotation

--- src/main/java/br/com/ottimizza/integradorcloud/controllers/v1/RegrasController.java
+++ src/main/java/br/com/ottimizza/integradorcloud/controllers/v1/RegrasController.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class RegrasController {
     RegraService regraService;
 
     @PostMapping
-    public ResponseEntity<?> criarRegra(GrupoRegra grupoRegraDTO, OAuth2Authentication authentication) throws Exception {
+    public ResponseEntity<?> criarRegra(@RequestBody grupoRegraDTO, OAuth2Authentication authentication) throws Exception {
         return ResponseEntity.ok(new GenericResponse<>(
             regraService.salvar(grupoRegraDTO, authentication)
         ));
