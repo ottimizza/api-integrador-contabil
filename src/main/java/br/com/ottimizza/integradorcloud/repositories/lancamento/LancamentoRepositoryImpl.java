529dbb6bcd398a3e00246702889371515e9df1ff
Lucas Martins <dev.lucasmartins@gmail.com>
2020-01-24 11:15:45 -0300

    adjustments apllied to update

--- src/main/java/br/com/ottimizza/integradorcloud/repositories/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/integradorcloud/repositories/lancamento/LancamentoRepositoryImpl.java
@@ -1,6 +1,5 @@
 package br.com.ottimizza.integradorcloud.repositories.lancamento;
 
-import java.lang.reflect.Field;
 import java.security.Principal;
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -18,11 +17,8 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
-import org.hibernate.Criteria;
-import org.hibernate.internal.CriteriaImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 
@@ -55,7 +51,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryCustom {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Lancamento> query = cb.createQuery(Lancamento.class);
         Root<Lancamento> root = query.from(Lancamento.class);
-        
+
         List<Predicate> predicates = predicates(regras, cb, root);
         predicates.add(0, cb.equal(root.get("cnpjEmpresa"), cnpjEmpresa));
 
@@ -69,72 +65,21 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryCustom {
         return new PageImpl<>(typedQuery.getResultList(), pageable, contarLancamentosPorRegra(regras, cnpjEmpresa));
     }
 
-    private CriteriaQuery<Lancamento> filter(CriteriaQuery<Lancamento> query, CriteriaBuilder cb, Root<Lancamento> root,
-            Regra regra) {
-        Path<String> path = root.get(regra.getCampo());
-        switch (regra.getCondicao()) {
-        case Regra.Condicao.CONTEM:
-            query = query.where(cb.like(cb.upper(path), MessageFormat.format("%{0}%", regra.getValor().toUpperCase())));
-            break;
-        case Regra.Condicao.NAO_CONTEM:
-            query = query.where(cb.notLike(path, MessageFormat.format("%{0}%", regra.getValor())));
-            break;
-        case Regra.Condicao.COMECAO_COM:
-            query = query.where(cb.like(path, MessageFormat.format("%{0}", regra.getValor())));
-            break;
-        case Regra.Condicao.IGUAL:
-            query = query.where(cb.equal(path, MessageFormat.format("{0}", regra.getValor())));
-            break;
-        }
-        return query;
-    }
+    @Modifying
+    @Transactional
+    public int atualizaLancamentosPorRegra(List<Regra> regras, String cnpjEmpresa, String contaMovimento) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaUpdate<Lancamento> update = cb.createCriteriaUpdate(Lancamento.class);
+        Root<Lancamento> root = update.from(Lancamento.class);
 
-    private <T> CriteriaQuery<T> filter(Regra regra, CriteriaQuery<T> query, CriteriaBuilder cb, Root<Lancamento> root,
-            Class<T> clazz) {
-        Path<String> path = root.get(regra.getCampo());
-        switch (regra.getCondicao()) {
-        case Regra.Condicao.CONTEM:
-            query = query
-                    .where(cb.like(unaccent(cb, path), MessageFormat.format("%{0}%", regra.getValor()).toUpperCase()));
-            break;
-        case Regra.Condicao.NAO_CONTEM:
-            query = query.where(
-                    cb.notLike(unaccent(cb, path), MessageFormat.format("%{0}%", regra.getValor()).toUpperCase()));
-            break;
-        case Regra.Condicao.COMECAO_COM:
-            query = query
-                    .where(cb.like(unaccent(cb, path), MessageFormat.format("%{0}", regra.getValor()).toUpperCase()));
-            break;
-        case Regra.Condicao.IGUAL:
-            query = query
-                    .where(cb.equal(unaccent(cb, path), MessageFormat.format("{0}", regra.getValor()).toUpperCase()));
-            break;
-        }
-        return query;
-    }
+        List<Predicate> predicates = predicates(regras, cb, root);
+        predicates.add(0, cb.equal(root.get("cnpjEmpresa"), cnpjEmpresa));
 
-    private <T> CriteriaUpdate<T> filter(Regra regra, CriteriaUpdate<T> query, CriteriaBuilder cb,
-            Root<Lancamento> root, Class<T> clazz) {
-        Path<String> path = root.get(regra.getCampo());
-        switch (regra.getCondicao()) {
-        case Regra.Condicao.CONTEM:
-            query = query
-                    .where(cb.like(unaccent(cb, path), MessageFormat.format("%{0}%", regra.getValor()).toUpperCase()));
-            break;
-        case Regra.Condicao.NAO_CONTEM:
-            query = query.where(
-                    cb.notLike(unaccent(cb, path), MessageFormat.format("%{0}%", regra.getValor()).toUpperCase()));
-            break;
-        case Regra.Condicao.COMECAO_COM:
-            query = query
-                    .where(cb.like(unaccent(cb, path), MessageFormat.format("%{0}", regra.getValor()).toUpperCase()));
-            break;
-        case Regra.Condicao.IGUAL:
-            query = query
-                    .where(cb.equal(unaccent(cb, path), MessageFormat.format("{0}", regra.getValor()).toUpperCase()));
-            break;
-        }
-        return query;
+        update.set(root.get("contaMovimento"), contaMovimento);
+        update.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+
+        // perform update and return affected rows.
+        return this.em.createQuery(update).executeUpdate();
     }
 
     private List<Predicate> predicates(List<Regra> clauses, CriteriaBuilder cb, Root<Lancamento> root) { // @formatter:off
@@ -157,37 +102,6 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryCustom {
             }
         });
         return predicates;
-    }
-
-    @Modifying
-    @Transactional
-    public int atualizaLancamentosPorRegra(List<Regra> regras, String cnpjEmpresa, String contaMovimento) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-
-        CriteriaUpdate<Lancamento> update = cb.createCriteriaUpdate(Lancamento.class);
-        Root<Lancamento> root = update.from(Lancamento.class);
-
-        update.set(root.get("contaMovimento"), contaMovimento);
-        update.where(cb.equal(root.get("cnpjEmpresa"), cnpjEmpresa));
-
-        for (Regra regra : regras) {
-            update = filter(regra, update, cb, root, Lancamento.class);
-        }
-
-        // perform update and return affected rows.
-        return this.em.createQuery(update).executeUpdate();
-    }
-
-    private Field getField(String fieldName) throws NoSuchFieldException {
-        return Lancamento.class.getField(fieldName);
-    }
-
-    private Object getFieldType(Field field) {
-        return field.getType();
-    }
-
-    private Object getFieldValue(Field field, Lancamento instance) throws IllegalAccessException {
-        return field.get(instance);
     }
 
     private Expression<String> unaccent(CriteriaBuilder cb, Path<String> path) {
