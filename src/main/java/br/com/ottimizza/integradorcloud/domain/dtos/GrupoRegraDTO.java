00c2bb0c647c64ab7487aa434021bfc590f5d473
Lucas Martins <dev.lucasmartins@gmail.com>
2020-02-13 11:26:06 -0300

    created GrupoRegraDTO

---
+++ src/main/java/br/com/ottimizza/integradorcloud/domain/dtos/GrupoRegraDTO.java
@@ -0,0 +1,44 @@
+package br.com.ottimizza.integradorcloud.domain.dtos;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import br.com.ottimizza.integradorcloud.domain.models.Regra;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class GrupoRegraDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private BigInteger id;
+
+    private Integer posicao;
+    
+    private String contaMovimento;
+
+    private Short tipoLancamento;
+
+    private String idRoteiro;
+
+    private String cnpjEmpresa;
+
+    private String cnpjContabilidade;
+
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataAtualizacao;
+
+    private List<Regra> regras;
+    
+}
--- src/main/java/br/com/ottimizza/integradorcloud/services/RegraService.java
+++ src/main/java/br/com/ottimizza/integradorcloud/services/RegraService.java
@@ -2,14 +2,24 @@ package br.com.ottimizza.integradorcloud.services;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import javax.inject.Inject;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 
+import br.com.ottimizza.integradorcloud.domain.criterias.PageCriteria;
+import br.com.ottimizza.integradorcloud.domain.dtos.GrupoRegraDTO;
+import br.com.ottimizza.integradorcloud.domain.mappers.grupo_regra.GrupoRegraMapper;
 import br.com.ottimizza.integradorcloud.domain.models.GrupoRegra;
+import br.com.ottimizza.integradorcloud.domain.models.Lancamento;
 import br.com.ottimizza.integradorcloud.domain.models.Regra;
 import br.com.ottimizza.integradorcloud.repositories.grupo_regra.GrupoRegraRepository;
 import br.com.ottimizza.integradorcloud.repositories.lancamento.LancamentoRepository;
@@ -27,12 +37,28 @@ public class RegraService {
     @Inject
     RegraRepository regraRepository;
 
-    public String salvar(GrupoRegra grupoRegraDTO, OAuth2Authentication authentication) throws Exception {
+    public List<GrupoRegraDTO> buscarRegras(GrupoRegraDTO filtro, PageCriteria pageCriteria, OAuth2Authentication authentication) 
+            throws Exception {
+        ExampleMatcher matcher = ExampleMatcher.matching().withStringMatcher(StringMatcher.CONTAINING);
+        Example<GrupoRegra> example = Example.of(GrupoRegraMapper.fromDto(filtro), matcher);
+
+        Page<GrupoRegra> grupoRegras = grupoRegraRepository.findAll(example, PageCriteria.getPageRequest(pageCriteria));
+
+        return new ArrayList<>();
+    }
+
+    public String salvar(GrupoRegraDTO grupoRegraDTO, OAuth2Authentication authentication) throws Exception {
         String message = "";
+
+        validaGrupoRegra(grupoRegraDTO);
+
+        Integer posicao = grupoRegraRepository.buscarUltimaPosicaoPorEmpresaETipoLancamento(
+            grupoRegraDTO.getCnpjEmpresa(), grupoRegraDTO.getTipoLancamento()
+        );
 
         List<Regra> regras = grupoRegraDTO.getRegras();
 
-        GrupoRegra grupoRegra = grupoRegraRepository.save(grupoRegraDTO);
+        GrupoRegra grupoRegra = grupoRegraRepository.save(GrupoRegraMapper.fromDto(grupoRegraDTO));
 
         System.out.println("Count >> " + regras.size());
 
@@ -57,6 +83,31 @@ public class RegraService {
         }
 
         return message;
+    }
+
+    boolean validaGrupoRegra(GrupoRegraDTO grupoRegraDTO) throws IllegalArgumentException {
+        String idRoteiro = grupoRegraDTO.getIdRoteiro();
+        String contaMovimento = grupoRegraDTO.getContaMovimento();
+        Short tipoLancamento = grupoRegraDTO.getTipoLancamento();
+        String cnpjEmpresa = grupoRegraDTO.getCnpjEmpresa();
+        String cnpjContabilidade = grupoRegraDTO.getCnpjContabilidade();
+
+        if (Objects.isNull(cnpjContabilidade) || cnpjContabilidade.isEmpty()) {
+            throw new IllegalArgumentException("Informe o CNPJ da Contabilidade!");
+        }
+        if (Objects.isNull(cnpjEmpresa) || cnpjEmpresa.isEmpty()) {
+            throw new IllegalArgumentException("Informe o CNPJ da Empresa!");
+        }
+        if (Objects.isNull(contaMovimento) || contaMovimento.isEmpty()) {
+            throw new IllegalArgumentException("Informe a Conta Movimento!");
+        }
+        if (!Arrays.asList(Lancamento.Tipo.PAGAMENTO, Lancamento.Tipo.RECEBIMENTO).contains(tipoLancamento)) {
+            throw new IllegalArgumentException("Informe o Tipo de Lançamento! Pagamentos(1) ou Recebimentos(2).");
+        }
+        if (Objects.isNull(idRoteiro) || idRoteiro.isEmpty()) {
+            throw new IllegalArgumentException("Informe a Id do Roteiro!");
+        }
+        return true;
     }
 
 }
