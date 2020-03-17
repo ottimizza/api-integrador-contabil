fbf45995ccbba4b6ef6b2360594d812b2a09e4c0
Felipe Voigt <felipe.voigt.12@gmail.com>
2020-03-17 17:19:22 -0300

    ultima versao mandando em lote p salesForce

--- src/main/java/br/com/ottimizza/integradorcloud/controllers/v1/SalesForceApiController.java
+++ src/main/java/br/com/ottimizza/integradorcloud/controllers/v1/SalesForceApiController.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -12,6 +14,7 @@ import org.h2.util.json.JSONObject;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,11 +28,14 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.com.ottimizza.integradorcloud.client.SalesForceClient;
 import br.com.ottimizza.integradorcloud.domain.dtos.grupo_regra.GrupoRegraDTO;
 import br.com.ottimizza.integradorcloud.domain.dtos.sfparticularidade.SFParticularidade;
 import br.com.ottimizza.integradorcloud.domain.mappers.grupo_regra.GrupoRegraMapper;
 import br.com.ottimizza.integradorcloud.domain.models.GrupoRegra;
+import br.com.ottimizza.integradorcloud.domain.models.Regra;
 import br.com.ottimizza.integradorcloud.domain.responses.GenericResponse;
 import br.com.ottimizza.integradorcloud.repositories.grupo_regra.GrupoRegraRepository;
 import br.com.ottimizza.integradorcloud.repositories.regra.RegraRepository;
@@ -39,50 +45,52 @@ import br.com.ottimizza.integradorcloud.services.RegraService;
 @RequestMapping("/api/sf")
 public class SalesForceApiController {
 
-	@Inject 
+	@Inject
 	SalesForceClient salesForceClient;
-	
+
 	@Inject
 	GrupoRegraRepository grupoRegraRepository;
-	
+
 	@Inject
 	RegraRepository regraRepository;
-	
+
 	@Inject
 	RegraService regraService;
-	
+
 	@PatchMapping("/patch/{id}")
-	public ResponseEntity<String> patch(@PathVariable BigInteger id, @RequestHeader("Authorization") String authorization) throws Exception {
+	public ResponseEntity<String> patch(@PathVariable BigInteger id,
+			@RequestHeader("Authorization") String authorization) throws Exception {
 		GrupoRegra grupoRegra = grupoRegraRepository.findById(id).get();
 		grupoRegra.setRegras(regraRepository.buscarPorGrupoRegra(id));
-		
+
 		SFParticularidade sfParticularidade = GrupoRegraMapper.toSalesForce(grupoRegra);
-		
+
 		return salesForceClient.upsert(id, sfParticularidade, authorization);
 	}
-	
+
 	@PostMapping("/importar")
-	public ResponseEntity<?> post(@Valid GrupoRegraDTO filter, @RequestHeader("Authorization") String authorization, OAuth2Authentication authentication) throws Exception {
-		if(filter.getCnpjEmpresa() == null || filter.getCnpjEmpresa() == "") {
+	public ResponseEntity<?> post(@Valid GrupoRegraDTO filter, @RequestHeader("Authorization") String authorization,
+			OAuth2Authentication authentication) throws Exception {
+		if (filter.getCnpjEmpresa() == null || filter.getCnpjEmpresa() == "") {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cnpj da empresa obrigatório para o envio!");
 		}
 		List<GrupoRegra> listaGrupoRegras = regraService.findToSalesForce(filter, authentication);
 
-		for(GrupoRegra grupoRegra : listaGrupoRegras) {
+		for (GrupoRegra grupoRegra : listaGrupoRegras) {
 			
-			BigInteger grupoRegraId = grupoRegra.getId();
-			grupoRegra.setRegras(regraRepository.buscarPorGrupoRegra(grupoRegraId));
-			SFParticularidade particularidade =  GrupoRegraMapper.toSalesForce(grupoRegra);
+			List<Regra> regras = regraRepository.buscarPorGrupoRegra(grupoRegra.getId());
 			
-			salesForceClient.upsert(grupoRegraId, particularidade, authorization);
+			SFParticularidade particularidade = GrupoRegraMapper.toSalesForce(grupoRegra, regras, false);
+
+			salesForceClient.upsert(grupoRegra.getId(), particularidade, authorization);
 		}
-		
+
 		GenericResponse<GrupoRegra> resposta = new GenericResponse<GrupoRegra>();
+		resposta.setRecords(listaGrupoRegras);
 		resposta.setMessage("Enviado regras com sucesso!");
 		resposta.setStatus("success");
-		
+
 		return ResponseEntity.ok(resposta);
 	}
-	
-	
+
 }
