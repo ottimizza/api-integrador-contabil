41f422a8cbe1f69e705176e131e1a95a53983ad8
Daniel Steil <daniel.steil@hotmail.com>
2020-08-17 14:46:53 -0300

    adicionando validacao do historico

--- src/main/java/br/com/ottimizza/integradorcloud/services/HistoricoService.java
+++ src/main/java/br/com/ottimizza/integradorcloud/services/HistoricoService.java
@@ -49,16 +49,18 @@ public class HistoricoService {
     	return "Historico removido com sucesso!";
     }
     
-    public HistoricoDTO atualizar(BigInteger id, HistoricoDTO historico, OAuth2Authentication authentication) throws Exception {
+    public HistoricoDTO atualizar(BigInteger id, HistoricoDTO historicoDTO, OAuth2Authentication authentication) throws Exception {
     	Historico existente = historicoRepository.findById(id).orElseThrow(() -> new NoResultException("Historico não encontrada!"));
-    	historico.setCnpjContabilidade(existente.getCnpjContabilidade());
-    	historico.setCnpjEmpresa(existente.getCnpjEmpresa());
-    	historico.setContaMovimento(existente.getContaMovimento());
-    	historico.setTipoLancamento(existente.getTipoLancamento());
-    	historico.setIdRoteiro(existente.getIdRoteiro());
-    	historico.setDataCriacao(existente.getDataCriacao());
+    	historicoDTO.setCnpjContabilidade(existente.getCnpjContabilidade());
+    	historicoDTO.setCnpjEmpresa(existente.getCnpjEmpresa());
+    	historicoDTO.setContaMovimento(existente.getContaMovimento());
+    	historicoDTO.setTipoLancamento(existente.getTipoLancamento());
+    	historicoDTO.setIdRoteiro(existente.getIdRoteiro());
+    	historicoDTO.setDataCriacao(existente.getDataCriacao());
+    	Historico historico = HistoricoMapper.fromDto(historicoDTO);
     	
-    	return HistoricoMapper.fromEntity(historicoRepository.save(HistoricoMapper.fromDto(historico)));
+    	validaHistorico(historico);
+    	return HistoricoMapper.fromEntity(historicoRepository.save(historico));
     }
 
     public String getAuthorizationHeader(OAuth2Authentication authentication) {
@@ -71,5 +73,22 @@ public class HistoricoService {
         ExampleMatcher matcher = ExampleMatcher.matching().withStringMatcher(StringMatcher.EXACT);
         return Example.of(HistoricoMapper.fromDto(historicoDTO), matcher);
     }
+    
+    public boolean validaHistorico(Historico historico) throws Exception {
+    	if(historico.getContaMovimento() == null || historico.getContaMovimento().equals(""))
+    		throw new IllegalArgumentException("Informe a conta movimento relacionada ao historico!");
+    	if(historico.getHistorico() == null || historico.getHistorico().equals(""))
+    		throw new IllegalArgumentException("Informe a regra do historico!");
+    	if(historico.getCnpjEmpresa() == null || historico.getCnpjEmpresa().equals(""))
+    		throw new IllegalArgumentException("Informe o cnpj da empresa relacionado ao historico!");
+    	if(historico.getCnpjContabilidade() == null || historico.getCnpjContabilidade().equals(""))
+       		throw new IllegalArgumentException("Informe o cnpj da contabilidade relacionado ao historico!");
+    	if(historico.getTipoLancamento() == null)
+    		throw new IllegalArgumentException("Informe o tipo de lancamento relacionado ao historico!");
+    	if(historico.getIdRoteiro() == null || historico.getIdRoteiro().equals(""))
+    		throw new IllegalArgumentException("Informe o id de roteiro relacionado ao historico!");
+    	return true;
+    }
+
 
 }
