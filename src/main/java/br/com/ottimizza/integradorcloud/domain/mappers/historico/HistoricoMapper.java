27bad96b9677c570f613bf1f85203d5d0af21eed
Daniel Steil <daniel.steil@hotmail.com>
2020-08-20 17:32:19 -0300

    adicionando campos e texto ao objeto SfHistorico

--- src/main/java/br/com/ottimizza/integradorcloud/domain/mappers/historico/HistoricoMapper.java
+++ src/main/java/br/com/ottimizza/integradorcloud/domain/mappers/historico/HistoricoMapper.java
@@ -44,29 +44,39 @@ public class HistoricoMapper { // @formatter:off
     
     public static SFHistorico toSalesForce(HistoricoDTO historico) {
     	String codigoHistorico = "";
+    	
     	String texto01 = "";
-    	
     	String campo01 = "";
+    	
     	String texto02 = "";
-    	
     	String campo02 = "";
-    	String texto03 = "";
     	
+    	String texto03 = "";
     	String campo03 = "";
+    	
     	String texto04 = "";
+    	String campo04 = "";
+    	
+    	String texto05 = "";
+    	String campo05 = "";
     	
     	String[] tudo = historico.getHistorico().split("\\$");
     	codigoHistorico = tudo[0].replace("CodigoHistorico:", "").trim();
+    	
     	texto01 = tudo[1].trim();
-    	
     	campo01 = tudo[2].substring(tudo[2].indexOf("{")+1,tudo[2].indexOf("}"));
+		
     	texto02 = tudo[2].substring(tudo[2].indexOf("}")+1);
-    	
     	campo02 = tudo[3].substring(tudo[3].indexOf("{")+1,tudo[3].indexOf("}"));
+		
     	texto03 = tudo[3].substring(tudo[3].indexOf("}")+1);
-    	
     	campo03 = tudo[4].substring(tudo[4].indexOf("{")+1,tudo[4].indexOf("}"));
+		
     	texto04 = tudo[4].substring(tudo[4].indexOf("}")+1);
+    	campo04 = tudo[5].substring(tudo[5].indexOf("{")+1,tudo[5].indexOf("}"));
+		
+    	texto05 = tudo[5].substring(tudo[5].indexOf("}")+1);
+    	campo05 = tudo[6].substring(tudo[6].indexOf("{")+1,tudo[6].indexOf("}"));
     	
     	
     	return SFHistorico.builder()
@@ -82,7 +92,10 @@ public class HistoricoMapper { // @formatter:off
     			.Texto_03__c(texto03)
     			.Campo_03__c(StringUtils.trataCampoHistoricoProSalesForce(campo03))
     			.Texto_04__c(texto04)
-    			.build();
+    			.Campo_04__c(StringUtils.trataCampoHistoricoProSalesForce(campo04))
+    			.Texto_05__c(texto05)
+    			.Campo_05__c(StringUtils.trataCampoHistoricoProSalesForce(campo05))
+    		.build();
     }
     
 
