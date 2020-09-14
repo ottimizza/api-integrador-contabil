f926263578b81ce6c8d23273127371aee592bee7
Daniel Steil <dani.steil@hotmail.com>
2020-09-14 13:52:54 -0300

    agora vai

--- src/main/java/br/com/ottimizza/integradorcloud/domain/models/checklist/CheckListPerguntas.java
+++ src/main/java/br/com/ottimizza/integradorcloud/domain/models/checklist/CheckListPerguntas.java
@@ -3,6 +3,15 @@ package br.com.ottimizza.integradorcloud.domain.models.checklist;
 
 import java.math.BigInteger;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,20 +20,33 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "checklist_perguntas")
 public class CheckListPerguntas {
 
+	@Id
+	@SequenceGenerator(name = "perguntas_sequence", sequenceName = "perguntas_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "perguntas_sequence")
+	@Column(name = "id")
 	private BigInteger id;
 	
+	@Column(name = "descricao")
 	private String descricao;
 	
+	@Column(name = "tipo")
 	private Short tipo;
 	
+	@Column(name = "opcoes_respostas")
+	@Convert(converter = PerguntasOpcoesRespostaConverter.class)
 	private PerguntasOpcoesResposta opcoesResposta;
 	
+	@Column(name = "valor")
 	private String valor;
 	
+	@Column(name = "sugestao")
 	private String sugestao;
 	
+	@Column(name = "grupo")
 	private Integer grupo;
 	
 }
---
+++ src/main/java/br/com/ottimizza/integradorcloud/repositories/checklist/CheckListRepository.java
@@ -0,0 +1,13 @@
+package br.com.ottimizza.integradorcloud.repositories.checklist;
+
+import java.math.BigInteger;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.ottimizza.integradorcloud.domain.models.checklist.CheckListRespostas;
+
+@Repository
+public interface CheckListRepository extends JpaRepository<CheckListRespostas, BigInteger>{
+
+}
