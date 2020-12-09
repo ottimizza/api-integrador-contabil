bbdbde313c984cea95e016df9bb382eefe65dc45
Daniel Steil <dani.steil@hotmail.com>
2020-12-09 13:30:10 -0300

    tabela regrasIgnoradas

---
+++ src/main/java/br/com/ottimizza/integradorcloud/domain/models/GrupoRegraIgnorada.java
@@ -0,0 +1,84 @@
+package br.com.ottimizza.integradorcloud.domain.models;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import com.vladmihalcea.hibernate.type.array.ListArrayType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@TypeDef(
+        name = "list-array",
+        typeClass = ListArrayType.class
+)
+@Data
+@Entity
+@Table(name = "grupo_regras_ignoradas")
+@NoArgsConstructor
+@AllArgsConstructor
+public class GrupoRegraIgnorada implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id", nullable = false)
+	private BigInteger id;
+
+	@Column(name = "posicao")
+	private Integer posicao;
+
+	@Column(name = "conta_movimento")
+	private String contaMovimento;
+
+	@Column(name = "tipo_lancamento")
+	private Short tipoLancamento;
+
+	@Column(name = "id_roteiro")
+	private String idRoteiro;
+
+	@Column(name = "cnpj_empresa")
+	private String cnpjEmpresa;
+
+	@Column(name = "cnpj_contabilidade")
+	private String cnpjContabilidade;
+
+	@Column(name = "contagem_regras")
+	private Integer contagemRegras;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataAtualizacao;
+
+	@Transient
+	private List<Regra> regras;
+
+	@Column(columnDefinition = "boolean default true")
+	private Boolean ativo;
+
+	@Column(name = "usuario")
+	public String usuario;
+
+	@Type(type = "list-array")
+	@Column(name = "campos", columnDefinition = "varchar[]")
+	private List<String> camposRegras;
+}
