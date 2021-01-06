17110b95285b28e40e0881f85ddc80a3c2c6122b
Daniel Steil <dani.steil@hotmail.com>
2021-01-06 10:36:39 -0300

    .

--- src/main/java/br/com/ottimizza/integradorcloud/services/LancamentoService.java
+++ src/main/java/br/com/ottimizza/integradorcloud/services/LancamentoService.java
@@ -421,12 +421,12 @@ public class LancamentoService {
 		
 		if(restantes.getTotalElements() == 0 && total.getTotalElements() != 0) {
 			String quebraLinha = System.lineSeparator();
-			System.out.println(quebraLinha);
 			StringBuilder sb = new StringBuilder();
-			sb.append("Contabilidade: "+userInfo.getOrganization().getName()+quebraLinha);
-			sb.append("Empresa: "+empresa.getRazaoSocial()+quebraLinha);
-			sb.append("Processo: "+tipoMovimento+quebraLinha);
+			sb.append("Contabilidade: "+userInfo.getOrganization().getName()+"\r\n");
+			sb.append("Empresa: "+empresa.getRazaoSocial()+"\r\n");
+			sb.append("Processo: "+tipoMovimento+"\r\n");
 			sb.append("Finalizado por: "+userInfo.getFirstName()+" "+userInfo.getLastName());
+			System.out.println(sb.toString());
 			EmailDTO email = EmailDTO.builder()
 					.to("dani.steil@hotmail.com")
 					.subject("Empresa "+empresa.getRazaoSocial()+" com OUD finalizado.")
