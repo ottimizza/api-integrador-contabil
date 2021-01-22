ae50947f163b504ca482131f7e611f6d458d6da3
Daniel Steil <dani.steil@hotmail.com>
2021-01-22 09:27:00 -0300

    crud arquivoPronto finalizado

---
+++ src/main/java/br/com/ottimizza/integradorcloud/controllers/v1/ArquivoProntoController.java
@@ -0,0 +1,70 @@
+package br.com.ottimizza.integradorcloud.controllers.v1;
+
+import java.math.BigInteger;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ottimizza.integradorcloud.domain.commands.arquivo_pronto.ImportacaoArquivoPronto;
+import br.com.ottimizza.integradorcloud.domain.criterias.PageCriteria;
+import br.com.ottimizza.integradorcloud.domain.dtos.aquivo_pronto.ArquivoProntoDTO;
+import br.com.ottimizza.integradorcloud.domain.responses.GenericPageableResponse;
+import br.com.ottimizza.integradorcloud.domain.responses.GenericResponse;
+import br.com.ottimizza.integradorcloud.services.ArquivoProntoService;
+
+@RestController
+@RequestMapping("/api/v1/arquivo_pronto")
+public class ArquivoProntoController {
+	
+	@Inject
+	ArquivoProntoService service;
+	
+	@GetMapping
+	public ResponseEntity<?> buscaComFiltro(@Valid ArquivoProntoDTO arquivo,
+											@Valid PageCriteria criteria) throws Exception {
+		return ResponseEntity.ok(new GenericPageableResponse<>(
+				service.buscaComFiltro(arquivo, criteria)
+			));
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> salvaArquivoPronto(@RequestBody ArquivoProntoDTO arquivo) throws Exception {
+		return ResponseEntity.ok(new GenericResponse<>(
+				service.salvaArquivo(arquivo)
+			));
+	}
+	
+	@PostMapping("/importar")
+	public ResponseEntity<?> importarArquivosProntos(@RequestBody ImportacaoArquivoPronto arquivos) throws Exception {
+		return ResponseEntity.ok(new GenericResponse<>(
+				service.importarArquivos(arquivos)
+			));
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<?> atualizaArquivoPronto(@PathVariable BigInteger id,
+												   @RequestBody ArquivoProntoDTO arquivo) throws Exception {
+		return ResponseEntity.ok(new GenericResponse<>(
+				service.patchArquivo(id, arquivo)
+			));
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<?> deletaArquivoPronto(@PathVariable BigInteger id) throws Exception {
+		return ResponseEntity.ok(new GenericResponse<>(
+				service.deletaArquivo(id)
+			));
+	}
+	
+
+}
--- src/main/java/br/com/ottimizza/integradorcloud/services/ArquivoProntoService.java
+++ src/main/java/br/com/ottimizza/integradorcloud/services/ArquivoProntoService.java
@@ -1,9 +1,18 @@
 package br.com.ottimizza.integradorcloud.services;
 
-import javax.inject.Inject;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+import javax.persistence.NoResultException;
+
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import br.com.ottimizza.integradorcloud.domain.commands.arquivo_pronto.ImportacaoArquivoPronto;
+import br.com.ottimizza.integradorcloud.domain.criterias.PageCriteria;
 import br.com.ottimizza.integradorcloud.domain.dtos.aquivo_pronto.ArquivoProntoDTO;
 import br.com.ottimizza.integradorcloud.domain.mappers.arquivo_pronto.ArquivoProntoMapper;
 import br.com.ottimizza.integradorcloud.domain.models.ArquivoPronto;
@@ -19,6 +28,32 @@ public class ArquivoProntoService {
 	public ArquivoProntoDTO salvaArquivo(ArquivoProntoDTO arquivo) throws Exception {
 		ArquivoPronto retorno = repository.save(ArquivoProntoMapper.fromDTO(arquivo));
 		return ArquivoProntoMapper.fromEntity(retorno);
+	}
+	
+	public List<ArquivoPronto> importarArquivos(ImportacaoArquivoPronto importacaoArquivos) throws Exception {
+		List<ArquivoPronto> retorno = new ArrayList<ArquivoPronto>();
+		List<ArquivoPronto> arquivos = importacaoArquivos.getArquivos().stream().map((arquivo) -> {
+			return ArquivoProntoMapper.fromDTO(arquivo).toBuilder()
+					.cnpjContabilidade(importacaoArquivos.getCnpjContabilidade())
+					.cnpjEmpresa(importacaoArquivos.getCnpjEmpresa())
+					.build();
+		}).collect(Collectors.toList());
+		repository.saveAll(arquivos).forEach(retorno::add);
+		return retorno;
+	}
+	
+	public ArquivoProntoDTO patchArquivo(BigInteger id, ArquivoProntoDTO arquivoDTO) throws Exception {
+		ArquivoPronto arquivo = repository.findById(id).orElseThrow(() -> new NoResultException("Arquivo protno nao encontrado!"));
+		return ArquivoProntoMapper.fromEntity(repository.save(arquivoDTO.patch(arquivo)));
+	}
+	
+	public Page<ArquivoPronto> buscaComFiltro(ArquivoProntoDTO filtro, PageCriteria criteria) throws Exception {
+		return repository.buscaComFiltro(filtro, criteria);
+	}
+	
+	public String deletaArquivo(BigInteger id) throws Exception {
+		repository.deleteById(id);
+		return "Arquivo pronto removido com sucesso!";
 	}
 
 }
