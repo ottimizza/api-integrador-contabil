74df4866a0d67f402e9c168e1af3bdcfde6cb6ed
Daniel Steil <dani.steil@hotmail.com>
2021-02-03 15:48:25 -0300

    alterando anotacao

--- src/main/java/br/com/ottimizza/integradorcloud/client/OttimizzaS1Client.java
+++ src/main/java/br/com/ottimizza/integradorcloud/client/OttimizzaS1Client.java
@@ -2,6 +2,7 @@ package br.com.ottimizza.integradorcloud.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import feign.Param;
 
@@ -9,9 +10,9 @@ import feign.Param;
 public interface OttimizzaS1Client {
 
 	@GetMapping("/contabil-server/roteiro.jsp?idRoteiro={idRoteiro}&codigoRoteiro={codRoteiro}&empresa={empresa}&tipo={tipo}&path=Deploy")
-	public void exportarRegras(@Param("idRoteiro") String idRoteiro, 
-			                   @Param("codRoteiro") String codigoRoteiro, 
-			                   @Param("empresa") String empresa, 
-			                   @Param("tipo") String tipoConta);
+	public void exportarRegras(@PathVariable("idRoteiro") String idRoteiro, 
+			@PathVariable("codRoteiro") String codigoRoteiro, 
+			@PathVariable("empresa") String empresa, 
+			                   @PathVariable("tipo") String tipoConta);
 	
 }
