026d20a28d8e6d9e80b86d4dfc47c24a5385e0bb
Daniel Steil <dani.steil@hotmail.com>
2021-02-09 16:06:20 -0300

    loteProcessado finalizado

--- src/main/java/br/com/ottimizza/integradorcloud/repositories/lote_processado/LoteProcessadoRepositoryCustom.java
+++ src/main/java/br/com/ottimizza/integradorcloud/repositories/lote_processado/LoteProcessadoRepositoryCustom.java
@@ -1,5 +1,13 @@
 package br.com.ottimizza.integradorcloud.repositories.lote_processado;
 
+import org.springframework.data.domain.Page;
+
+import br.com.ottimizza.integradorcloud.domain.criterias.PageCriteria;
+import br.com.ottimizza.integradorcloud.domain.dtos.lote_processado.LoteProcessadoDTO;
+import br.com.ottimizza.integradorcloud.domain.models.LoteProcessado;
+
 public interface LoteProcessadoRepositoryCustom {
 
+	Page<LoteProcessado> buscaComFiltro(LoteProcessadoDTO filtro, PageCriteria criteria);
+	
 }
