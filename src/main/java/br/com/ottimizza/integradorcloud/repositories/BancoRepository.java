b4e0092efffac0e1647941a4a0f0e71a8e2bb533
Daniel Steil <dani.steil@hotmail.com>
2021-02-17 17:55:59 -0300

    finalizando order by

--- src/main/java/br/com/ottimizza/integradorcloud/repositories/BancoRepository.java
+++ src/main/java/br/com/ottimizza/integradorcloud/repositories/BancoRepository.java
@@ -3,9 +3,11 @@ package br.com.ottimizza.integradorcloud.repositories;
 import java.math.BigInteger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.ottimizza.integradorcloud.domain.models.Banco;
 
+@Repository
 public interface BancoRepository extends JpaRepository<Banco, BigInteger>{
 
 }
