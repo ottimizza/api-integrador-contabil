ca51bb304cff3fe6efee87918e324f6f6fa39bca
Daniel Steil <dani.steil@hotmail.com>
2021-03-03 09:14:36 -0300

    enableFeingClients

--- src/main/java/br/com/ottimizza/integradorcloud/IntegradorCloudApplication.java
+++ src/main/java/br/com/ottimizza/integradorcloud/IntegradorCloudApplication.java
@@ -2,8 +2,10 @@ package br.com.ottimizza.integradorcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class IntegradorCloudApplication {
 
 	public static void main(String[] args) {
